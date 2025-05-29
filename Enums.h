2dd426a7874c72728de833a6aab42e4d89c100b5
Danny <catanoandres131@gmail.com>
2025-05-29 16:32:29 -0500

    Clase Enums

---
+++ Enums.h
@@ -0,0 +1,13 @@
+#pragma once
+
+enum class ColorCarta {
+    ROJO, AZUL, VERDE, MORADO, GRIS, NARANJA, COMODIN, NINGUNO
+};
+
+enum class AccionJugador {
+    ROBAR_CARTAS, CONSTRUIR_TRAMO, TOMAR_COMODIN
+};
+
+enum class TipoCarta {
+    COLOR, COMODIN
+};
